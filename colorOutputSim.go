57cb5793eea3a8ea29f7e275f7dc1204f0100ef2
Murali <muralib@cs.cmu.edu>
2015-06-27 17:26:11 -0700

    Working go routine based Simulator

--- colorOutputSim.go
+++ colorOutputSim.go
@@ -12,7 +12,6 @@ import (
 	"math/rand"
 	"os"
 	"runtime"
-	"time"
 )
 
 const gridSize = 32
@@ -36,9 +35,9 @@ var agents [nAgents]Agent
 
 func main() {
 	runtime.GOMAXPROCS(10)
-	rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(10)
 	nWorkers := 4
-	timeSteps := 10
+	timeSteps := 100
 	pObs := 0.05
 	// Environment is going to have -2 unoccupied, -1 obstacle and index of agent
 
@@ -54,7 +53,7 @@ func main() {
 	}
 	// place the agents
 	for i := 0; i < nAgents; i++ {
-		colors[i] = color.RGBA{uint8(rand.Intn(128)), uint8(rand.Intn(128)), uint8(rand.Intn(128)), 64}
+		colors[i] = color.RGBA{uint8(rand.Intn(128)), uint8(rand.Intn(128)), uint8(rand.Intn(128)), 10}
 		agents[i].X = rand.Intn(gridSize)
 		agents[i].Y = rand.Intn(gridSize)
 		agents[i].num = i
@@ -70,13 +69,12 @@ func main() {
 	// create one channel per worked
 
 	jobs := make(chan int, nAgents)
-	resultX := make(chan int, nAgents)
-	resultY := make(chan int, nAgents)
+	results := make(chan Agent, nAgents)
 
 	for i := 0; i < nWorkers; i++ {
-		go worker(i, jobs, resultX, resultY)
+		go worker(i, jobs, results)
 	}
-	simulateLoop(timeSteps, jobs, resultX, resultY)
+	simulateLoop(timeSteps, jobs, results)
 	createGIF()
 	fmt.Println("Creating GIF")
 }
@@ -97,28 +95,30 @@ func createGIF() {
 	opfile.Close()
 }
 
-func worker(id int, jobs <-chan int, resultX chan int, resultY chan int) {
+func worker(id int, jobs <-chan int, results chan Agent) {
 	for j := range jobs {
-		updateAgent(j, resultX, resultY)
+		updateAgent(j, results)
 		fmt.Println("worker", id, "processed agent", j)
 	}
 }
 
-func simulateLoop(timeSteps int, jobs chan int, resultX chan int, resultY chan int) {
+func simulateLoop(timeSteps int, jobs chan int, results chan Agent) {
 
 	for t := 0; t < timeSteps; t++ {
+		fmt.Println("Time Step :", t)
 		for i := 0; i < nAgents; i++ {
 			// add agent to jobs
 			jobs <- i
 		}
 
 		for i := 0; i < nAgents; i++ {
-			Env[agents[i].X][agents[i].Y] = -2
-			tmpX := <-resultX
-			tmpY := <-resultY
-			agents[i].X = tmpX
-			agents[i].Y = tmpY
-			Env[agents[i].X][agents[i].Y] = i
+			tmp := <-results
+			j := tmp.num
+			Env[agents[j].X][agents[j].Y] = -2
+			//fmt.Println("Iter ", t, " Agent ", tmp.num, " X ", tmp.X, " Y ", tmp.Y)
+			agents[j].X = tmp.X
+			agents[j].Y = tmp.Y
+			Env[agents[j].X][agents[j].Y] = j
 		}
 		writeToJP()
 		if t+1 == timeSteps {
@@ -127,12 +127,14 @@ func simulateLoop(timeSteps int, jobs chan int, resultX chan int, resultY chan i
 	}
 }
 
-func updateAgent(i int, resultX chan int, resultY chan int) {
+func updateAgent(i int, results chan Agent) {
 	agent := agents[i]
 	// pick a direction at random
 	count := 0
-	MAXCOUNT := 20
+	MAXCOUNT := 200
 	flag := true
+	var outAgent Agent
+	outAgent.num = i
 	for flag {
 		dirX := 2 * (float64(rand.Intn(2)) - 0.5)
 		dirY := 2 * (float64(rand.Intn(2)) - 0.5)
@@ -140,17 +142,18 @@ func updateAgent(i int, resultX chan int, resultY chan int) {
 		newY := int(math.Min(math.Max(float64(agent.Y)+dirY, 0), float64(gridSize-1)))
 		// if unoccupied move there
 		if Env[newX][newY] == -2 {
-			resultX <- newX
-			resultY <- newY
+			outAgent.X = newX
+			outAgent.Y = newY
 			flag = false
 		}
 		if count > MAXCOUNT {
-			resultX <- agent.X
-			resultY <- agent.Y
+			outAgent.X = agent.X
+			outAgent.Y = agent.Y
 			flag = false
 		}
 		count = count + 1
 	}
+	results <- outAgent
 }
 
 func writeToJP() {
@@ -169,7 +172,7 @@ func writeToJP() {
 				fill = &image.Uniform{c}
 			}
 
-			draw.Draw(img, image.Rect((x-1)*10, (y-1)*10, x*10, y*10), fill, image.ZP, draw.Src)
+			draw.Draw(img, image.Rect(x*10, y*10, (x+1)*10, (y+1)*10), fill, image.ZP, draw.Src)
 		}
 	}
 	buf := bytes.Buffer{}
